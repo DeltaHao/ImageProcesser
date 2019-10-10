63e92fd6aa91aa5540520f0ddb620c39ad9605a1
Haozl <1219735011@qq.com>
2019-10-10 18:55:04 +0800

    可以转为灰度图

--- imageprocesser.cpp
+++ imageprocesser.cpp
@@ -34,6 +34,18 @@ void ImageProcesser::createActions(){
     //关闭按钮
     QAction *exitAct = fileMenu->addAction(tr("&关闭"), this, &QWidget::close);
     exitAct->setShortcut(tr("Ctrl+Q"));
+
+    //菜单栏-处理
+    QMenu *processMenu = menuBar()->addMenu(tr("&处理"));
+    //转为灰度图
+    changeToGrayAct = processMenu->addAction(tr("&查看灰度图"), this, &ImageProcesser::changeToGray);
+    changeToGrayAct->setEnabled(false);
+    changeToGrayAct->setCheckable(true);
+
+    //菜单栏-关于
+    QMenu *aboutMenu = menuBar()->addMenu(tr("&关于"));
+    QAction *about1Act = aboutMenu->addAction(tr("&关于作者..."), this, &ImageProcesser::about1);
+    QAction *about2Act = aboutMenu->addAction(tr("&项目介绍..."), this, &ImageProcesser::about2);
 }
 
 //初始化文件选择对话框
@@ -64,15 +76,15 @@ void ImageProcesser::open(){
 bool ImageProcesser::loadFile(const QString &fileName){
     QImageReader reader(fileName);
     reader.setAutoTransform(true);
-    const QImage newImage = reader.read();
-    if(newImage.isNull()){//如果图片不能打开， 报错
+    image = reader.read();
+    if(image.isNull()){//如果图片不能打开， 报错
         QMessageBox::information(this, QGuiApplication::applicationDisplayName(),
                                  tr("Cannot load %1: %2")
                                  .arg(QDir::toNativeSeparators(fileName), reader.errorString()));
         return false;
     }
 
-    setImage(newImage);
+    setImage(image);
 
     setWindowFilePath(fileName);
 
@@ -84,11 +96,13 @@ bool ImageProcesser::loadFile(const QString &fileName){
 //将图像显示在imageLabel上
 void ImageProcesser::setImage(const QImage &newImage){
     //将图像显示在imageLabel上
-    image = newImage;
-    imageLabel->setPixmap(QPixmap::fromImage(image));
+
+    imageLabel->setPixmap(QPixmap::fromImage(newImage));
     //以下控件可见或启用
     scrollArea->setVisible(true);
     saveAsAct->setEnabled(true);
+    changeToGrayAct->setEnabled(true);
+
     imageLabel->adjustSize();//imageLabel的大小可调整
 }
 //保存图片
@@ -113,4 +127,46 @@ void ImageProcesser::saveAs()
     initializeImageFileDialog(dialog);
 
     while (dialog.exec() == QDialog::Accepted && !saveFile(dialog.selectedFiles().first())) {}
+}
+//关于
+void ImageProcesser::about1(){
+    QMessageBox:: about(this, tr("作者"),tr("<p>作者：郝正亮</p><p>学号：1120172919</p>"));
+}
+void ImageProcesser::about2(){
+    QMessageBox:: about(this, tr("项目介绍"),tr("<p>《数字图像处理》课程作业</p> 。。。"));
+}
+//转化为灰度图像
+void ImageProcesser::toGray(const QImage image){
+    if(!grayimage.isNull())
+        setImage(grayimage);
+    int width = image.width();
+    int height = image.height();
+    QImage tmp(width, height, QImage::Format_Indexed8);//先建立一个同等尺寸的8位灰度图
+    //如果原图本来就是灰度图,直接拷贝
+    if(image.format() == QImage::Format_Indexed8){
+        tmp = image;
+    }
+    //设置灰度表
+    tmp.setColorCount(256);
+    for(int i=0;i<256;i++){
+        tmp.setColor(i,qRgb(i,i,i));
+    }
+    for(int i =0;i<width;i++) {
+         for(int j=0;j<height;j++)  {
+            QRgb pixel=image.pixel(i,j);
+            int r=qRed(pixel);
+            int g=qGreen(pixel);
+            int b=qBlue(pixel);
+            tmp.setPixel(i,j,qGray(r,g,b));
+         }
+    }
+    grayimage = tmp;
+    setImage(grayimage);
+}
+void ImageProcesser::changeToGray(){
+    bool isChecked = changeToGrayAct->isChecked();
+    if(!isChecked)
+        setImage(image);
+    else
+        toGray(image);
 }
--- imageprocesser.h
+++ imageprocesser.h
@@ -25,21 +25,24 @@ public:
 private slots://私有槽函数
     void open();//打开图片
     void saveAs();//另存为
-    //void about();//显示“关于”信息
+    void about1();//显示“关于作者”信息
+    void about2();//显示“关于软件”信息
+    void changeToGray();//转换为灰度图片
 private:
     //私有函数
     void createActions();//创造命令
-    void createMenus();//创造菜单界面
     bool saveFile(const QString &filename);
     void setImage(const QImage &newImage);
-
+    void toGray(QImage image);
     //实体成员
-    QImage image;
+    QImage image;//原图像
+    QImage grayimage;//灰度图像
     QLabel *imageLabel;
     QScrollArea *scrollArea;
 
     //一些要用到的命令
     QAction *saveAsAct;
+    QAction *changeToGrayAct;
 };
 
 #endif // IMAGEPROCESSER_H
