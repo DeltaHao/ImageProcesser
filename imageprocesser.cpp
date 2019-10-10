f37102f6945d54207d1a8dda99aa762faa10d49a
Haozl <1219735011@qq.com>
2019-10-10 21:08:16 +0800

    将8位灰度图转化为8幅位平面二值图

--- imageprocesser.cpp
+++ imageprocesser.cpp
@@ -38,9 +38,26 @@ void ImageProcesser::createActions(){
     //菜单栏-处理
     QMenu *processMenu = menuBar()->addMenu(tr("&处理"));
     //转为灰度图
-    changeToGrayAct = processMenu->addAction(tr("&查看灰度图"), this, &ImageProcesser::changeToGray);
+    changeToGrayAct = processMenu->addAction(tr("&灰度图/RGB图"), this, &ImageProcesser::changeToGray);
     changeToGrayAct->setEnabled(false);
     changeToGrayAct->setCheckable(true);
+    //转为位平面图
+    changeToBitplaneAct[0] = processMenu->addAction(tr("&位平面-1"), this, &ImageProcesser::changeToBitplane1);
+    changeToBitplaneAct[0]->setEnabled(false);
+    changeToBitplaneAct[1] = processMenu->addAction(tr("&位平面-2"), this, &ImageProcesser::changeToBitplane2);
+    changeToBitplaneAct[1]->setEnabled(false);
+    changeToBitplaneAct[2] = processMenu->addAction(tr("&位平面-3"), this, &ImageProcesser::changeToBitplane3);
+    changeToBitplaneAct[2]->setEnabled(false);
+    changeToBitplaneAct[3] = processMenu->addAction(tr("&位平面-4"), this, &ImageProcesser::changeToBitplane4);
+    changeToBitplaneAct[3]->setEnabled(false);
+    changeToBitplaneAct[4] = processMenu->addAction(tr("&位平面-5"), this, &ImageProcesser::changeToBitplane5);
+    changeToBitplaneAct[4]->setEnabled(false);
+    changeToBitplaneAct[5] = processMenu->addAction(tr("&位平面-6"), this, &ImageProcesser::changeToBitplane6);
+    changeToBitplaneAct[5]->setEnabled(false);
+    changeToBitplaneAct[6] = processMenu->addAction(tr("&位平面-7"), this, &ImageProcesser::changeToBitplane7);
+    changeToBitplaneAct[6]->setEnabled(false);
+    changeToBitplaneAct[7] = processMenu->addAction(tr("&位平面-8"), this, &ImageProcesser::changeToBitplane8);
+    changeToBitplaneAct[7]->setEnabled(false);
 
     //菜单栏-关于
     QMenu *aboutMenu = menuBar()->addMenu(tr("&关于"));
@@ -66,13 +83,13 @@ static void initializeImageFileDialog(QFileDialog &dialog){
     filters.append(" gifImages (*.gif)");
     dialog.setNameFilters(filters);
 }
+
 //打开图片
 void ImageProcesser::open(){
     QFileDialog dialog(this, tr("打开图片"));
     initializeImageFileDialog(dialog);
     while(dialog.exec() == QDialog::Accepted && !loadFile(dialog.selectedFiles().first())){}
 }
-//加载图片
 bool ImageProcesser::loadFile(const QString &fileName){
     QImageReader reader(fileName);
     reader.setAutoTransform(true);
@@ -84,6 +101,9 @@ bool ImageProcesser::loadFile(const QString &fileName){
         return false;
     }
 
+    toGray();//生成灰度图
+    toBitplane();//生成位平面图
+
     setImage(image);
 
     setWindowFilePath(fileName);
@@ -93,7 +113,6 @@ bool ImageProcesser::loadFile(const QString &fileName){
     statusBar()->showMessage(message);
     return true;
 }
-//将图像显示在imageLabel上
 void ImageProcesser::setImage(const QImage &newImage){
     //将图像显示在imageLabel上
 
@@ -102,9 +121,12 @@ void ImageProcesser::setImage(const QImage &newImage){
     scrollArea->setVisible(true);
     saveAsAct->setEnabled(true);
     changeToGrayAct->setEnabled(true);
+    for(int i=0; i<8; i++)
+        changeToBitplaneAct[i]->setEnabled(true);
 
     imageLabel->adjustSize();//imageLabel的大小可调整
 }
+
 //保存图片
 bool ImageProcesser::saveFile(const QString &fileName)
 {
@@ -120,7 +142,6 @@ bool ImageProcesser::saveFile(const QString &fileName)
     statusBar()->showMessage(message);
     return true;
 }
-//图片另存为(弹出窗口)
 void ImageProcesser::saveAs()
 {
     QFileDialog dialog(this, tr("图片另存为"));
@@ -128,6 +149,7 @@ void ImageProcesser::saveAs()
 
     while (dialog.exec() == QDialog::Accepted && !saveFile(dialog.selectedFiles().first())) {}
 }
+
 //关于
 void ImageProcesser::about1(){
     QMessageBox:: about(this, tr("作者"),tr("<p>作者：郝正亮</p><p>学号：1120172919</p>"));
@@ -135,10 +157,9 @@ void ImageProcesser::about1(){
 void ImageProcesser::about2(){
     QMessageBox:: about(this, tr("项目介绍"),tr("<p>《数字图像处理》课程作业</p> 。。。"));
 }
+
 //转化为灰度图像
-void ImageProcesser::toGray(const QImage image){
-    if(!grayimage.isNull())
-        setImage(grayimage);
+void ImageProcesser::toGray(){
     int width = image.width();
     int height = image.height();
     QImage tmp(width, height, QImage::Format_Indexed8);//先建立一个同等尺寸的8位灰度图
@@ -161,12 +182,53 @@ void ImageProcesser::toGray(const QImage image){
          }
     }
     grayimage = tmp;
-    setImage(grayimage);
 }
 void ImageProcesser::changeToGray(){
     bool isChecked = changeToGrayAct->isChecked();
-    if(!isChecked)
-        setImage(image);
-    else
-        toGray(image);
+    if(!isChecked)//若未被选择
+        imageLabel->setPixmap(QPixmap::fromImage(image));//显示原图
+
+    else//若被选择
+        imageLabel->setPixmap(QPixmap::fromImage(grayimage));//显示灰度图
+}
+
+//将8位灰度图像转换为8幅位平面二值图
+void ImageProcesser::toBitplane(){
+    int width =  image.width();
+    int height = image.height();
+
+    QImage tmp(width, height, QImage::Format_Mono);
+    for(int k=0;k<8;k++){
+        bitplaneimage[k] = tmp;
+        for(int i =0;i<width;i++) {
+             for(int j=0;j<height;j++)  {
+                int pixel = grayimage.pixelIndex(i, j);
+                bitplaneimage[k].setPixel(i,j,((pixel>>k) & 1));
+             }
+        }
+    }
+}
+void ImageProcesser::changeToBitplane1(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[0]));
+}
+void ImageProcesser::changeToBitplane2(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[1]));
+}
+void ImageProcesser::changeToBitplane3(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[2]));
+}
+void ImageProcesser::changeToBitplane4(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[3]));
+}
+void ImageProcesser::changeToBitplane5(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[4]));
+}
+void ImageProcesser::changeToBitplane6(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[5]));
+}
+void ImageProcesser::changeToBitplane7(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[6]));
+}
+void ImageProcesser::changeToBitplane8(){
+    imageLabel->setPixmap(QPixmap::fromImage(bitplaneimage[7]));
 }
--- imageprocesser.h
+++ imageprocesser.h
@@ -28,21 +28,34 @@ private slots://私有槽函数
     void about1();//显示“关于作者”信息
     void about2();//显示“关于软件”信息
     void changeToGray();//转换为灰度图片
+    //将8位灰度图像转换为8幅位平面二值图
+    void changeToBitplane1();
+    void changeToBitplane2();
+    void changeToBitplane3();
+    void changeToBitplane4();
+    void changeToBitplane5();
+    void changeToBitplane6();
+    void changeToBitplane7();
+    void changeToBitplane8();
 private:
     //私有函数
     void createActions();//创造命令
     bool saveFile(const QString &filename);
     void setImage(const QImage &newImage);
-    void toGray(QImage image);
+    void toGray();
+    void toBitplane();
+
     //实体成员
     QImage image;//原图像
     QImage grayimage;//灰度图像
+    QImage bitplaneimage[8];//8幅位平面二值图
     QLabel *imageLabel;
     QScrollArea *scrollArea;
 
     //一些要用到的命令
     QAction *saveAsAct;
     QAction *changeToGrayAct;
+    QAction *changeToBitplaneAct[8];
 };
 
 #endif // IMAGEPROCESSER_H
