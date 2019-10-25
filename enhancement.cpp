dd22257dc8abaf7646c85ed04503265ac8617fdc
Haozl <1219735011@qq.com>
2019-10-25 19:20:42 +0800

    完成图像锐化

--- enhancement.cpp
+++ enhancement.cpp
@@ -100,14 +100,92 @@ void ImageProcesser::gaussFilter(){
 //锐化
 void ImageProcesser::RobertsSharpen(){
     hideSpinBoxes();
+    int h = grayimage.height();
+    int w = grayimage.width();
 
+    QImage tmp = grayimage;
+    tmp.setColorCount(256);
+    for(int i=0;i<256;i++){
+        tmp.setColor(i,qRgb(i,i,i));
+    }
+
+    for(int i=1; i<w; i++){
+        for(int j=1; j<h; j++){
+            int Gx = grayimage.pixelIndex(i-1, j-1)-grayimage.pixelIndex(i, j);
+            int Gy = grayimage.pixelIndex(i-1, j)-grayimage.pixelIndex(i, j-1);
+            double sum = sqrt(Gx*Gx + Gy*Gy);
+
+            int index = (int)(sum+0.5);
+            if(index<0) index = 0;
+            if(index>255) index = 255;
+            tmp.setPixel(i ,j, index);
+        }
+    }
+    showImage(tmp);
 }
-
 void ImageProcesser::SobelSharpen(){
     hideSpinBoxes();
+
+    int h = grayimage.height();
+    int w = grayimage.width();
+
+    QImage tmp = grayimage;
+    tmp.setColorCount(256);
+    for(int i=0;i<256;i++){
+        tmp.setColor(i,qRgb(i,i,i));
+    }
+    int wights_x[3][3] = { -1, 0, +1, -2, 0, +2, -1, 0, +1 };
+    int wights_y[3][3] = { +1, +2, +1, 0, 0, 0, -1, -2, -1 };
+
+    for(int i=0; i<w; i++){
+        for(int j=0; j<h; j++){
+            int Gx=0, Gy=0;
+            for(int I=i-1; I<=i+1; I++){
+                for(int J=j-1; J<=j+1; J++){
+                    if(I<0 || J<0 || I>=w || J>=h) continue;
+                    Gx += (grayimage.pixelIndex(I, J) * wights_x[I-(i-1)][J-(j-1)]);
+                    Gy += (grayimage.pixelIndex(I, J) * wights_y[I-(i-1)][J-(j-1)]);
+                }
+            }
+            double sum = sqrt(Gx*Gx + Gy*Gy);
+
+            int index = (int)(sum+0.5);
+            if(index<0) index = 0;
+            if(index>255) index = 255;
+            tmp.setPixel(i ,j, index);
+        }
+    }
+    showImage(tmp);
 
 }
 void ImageProcesser::LaplacianSharpen(){
     hideSpinBoxes();
 
+    int h = grayimage.height();
+    int w = grayimage.width();
+
+    QImage tmp = grayimage;
+    tmp.setColorCount(256);
+    for(int i=0;i<256;i++){
+        tmp.setColor(i,qRgb(i,i,i));
+    }
+    int wights[3][3] = { -1, -1, -1, -1, 8, -1, -1, -1, -1 };
+
+    for(int i=0; i<w; i++){
+        for(int j=0; j<h; j++){
+            int sum = 0;
+            for(int I=i-1; I<=i+1; I++){
+                for(int J=j-1; J<=j+1; J++){
+                    if(I<0 || J<0 || I>=w || J>=h) continue;
+                    sum += grayimage.pixelIndex(I, J) * wights[I-(i-1)][J-(j-1)] ;
+                }
+            }
+            if(sum<0) sum = 0;
+            if(sum>255) sum = 255;
+            tmp.setPixel(i ,j, sum);
+        }
+    }
+    showImage(tmp);
 }
+
+//给定模版求卷积
